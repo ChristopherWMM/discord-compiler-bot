9a651054b6e11da0f4dd3a7004af4fe416bd12d9
Christopher Martin <Christopherwmm@gmail.com>
2020-10-17 15:08:49 -0700

    Update CompilationParser.js

--- src/commands/utils/CompilationParser.js
+++ src/commands/utils/CompilationParser.js
@@ -80,8 +80,8 @@ export default class CompilationParser {
             // pipe operator should be last, everything after it should be stdin
             else if (current == '|') {
                 const regex = /```([\s\S]*?)```/g;
-                regex.lastIndex = this.message.content.search('|');
-                let match = regex.exec(this.message.content);
+                regex.lastIndex = this.message.search('|');
+                let match = regex.exec(this.message);
                 if (match) {
                     argsData.fileInput = match[0];
                 } else {
